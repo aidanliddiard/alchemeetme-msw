f356ad36b2ab5016844f6c840d1884257279f835
aidanliddiard <aidanliddiard@gmail.com>
2022-04-27 14:35:06 -0700

    fist test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,16 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+import { rest } from 'msw'
 // import setupServer
+import { setupServer } from 'msw/node'
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,12 +22,21 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) =>
+    res(ctx.json([user]))
+  )
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
+
+afterEach(() => {
+  server.resetHandlers()
+})
 
 test('Should render the header', async () => {
   render(<App />)
@@ -57,6 +68,8 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   render(<App />)
 
   const profileName = await screen.findByText(sasuke.name)
+
+  screen.debug()
 
   expect(profileName).toBeInTheDocument()
 })
